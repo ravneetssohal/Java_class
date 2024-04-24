da8699a861188f395fa0af458e49363a129bdcff
Ravneet Sohal <ravneetsohal@Ravneets-MacBook-Air.local>
2024-04-24 03:56:50 -0400

    Corrected Logic

--- src/Assignments/LongestSubstring.java
+++ src/Assignments/LongestSubstring.java
@@ -3,12 +3,36 @@ package Assignments;
 import java.util.Scanner;
 
 public class LongestSubstring {
+
+    static public boolean duplicate(String subword){
+        for (int i = 0; i < subword.length(); i++) {
+            for (int j = i + 1; j < subword.length(); j++) {
+                if (subword.charAt(i) == subword.charAt(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the word you want to check: ");
         String word = scanner.next();
+        int max = 0;
+        int maxLength = 0;
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = i + 1; j < word.length(); j++) {
+                if(!duplicate(word.substring(i,j))){
+                   max = j - i;
+                   if(max > maxLength){
+                       maxLength = max;
+                   }
+                }
+            }
+        }
 
-        int max = 1;
+
+        /*int max = 1;
         int maxLength = 1;
         for (int i = 0; i < word.length() - 1; i++) {
             if(word.charAt(i) != word.charAt(i + 1)){
@@ -24,7 +48,7 @@ public class LongestSubstring {
                 }
                 max = 1;
             }
-        }
+        }*/
 
         System.out.println("Length of the longest substring without repeating characters is " + maxLength);
     }
