84db03ea6d5cf3a2881386106d83805fdec921ee
Ravneet Sohal <ravneetsohal@Ravneets-MacBook-Air.local>
2024-05-08 04:05:21 -0400

    Added homework

--- src/collections1/NoDuplicates.java
+++ src/collections1/NoDuplicates.java
@@ -1,18 +1,25 @@
 package collections1;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class NoDuplicates {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(0, 304, 50, 203, 503, 23, 30, 50, 203, 503, 22, 100);
 
-        for (int i = 0; i < list.size() ; i++) {
-            for (int j = 0; j < ; j++) {
-                
+        List<Integer> list1 = new ArrayList<>();
+
+        for(int element : list){
+            if(!list1.contains(element)){
+                list1.add(element);
             }
         }
+
+        System.out.println(list1);
+
+        //another method
+        Set<Integer> set = new HashSet<>(list);
+        System.out.println(set);
+
 
     }
 }
